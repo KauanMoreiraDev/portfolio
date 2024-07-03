d812ddabc430b05e71d6a56dcda3064f84c18d80
KauanMoreiraDev <kauanmoreira457@gmail.com>
2024-07-03 16:40:24 -0300

    hover in the links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,15 +31,15 @@ const contato = [{
 export default function Home() {
   return (
     <main className="bg-gradient-to-br from-primary via-secoundary to-pink-600 h-screen e w-screen flex items-center justify-center">
-      <div className="m-1 p-2 z-10 bg-lime-500 rounded-xl text-center flex flex-col flex-nowrap justify-evenly shadow-2xl w-1/3 h-2/3 max-sm:w-3/5 max-md:w-1/3 max-lg:w-1/3">
+      <div className="m-1 p-2 z-10 bg-lime-500 border-black border rounded-xl text-center flex flex-col flex-nowrap justify-evenly shadow-2xl w-1/3 h-2/3 max-sm:w-3/5 max-md:w-1/3 max-lg:w-1/3">
         <h1 className="font-Oswald text-3xl">Olá, eu sou o <span className="italic font-semibold">Kauan!</span></h1>
         <h2 className="p-2 font-Oswald font-Regular text-lg">Front-End Development Student</h2>
         <h2 className="p-2 font-Oswald font-medium">HTML | CSS | JS | Next.js | TailwindCSS</h2>
         <h3 className="p-2 font-Oswald font-Regular text-lg">Entre em contato</h3>
         <div className="pt-4 flex flex-row justify-center">
           {contato.map((cttfn, index) => (
-            <Link key={index} href={cttfn.link} target="_blank">
-              <Image className="p-1 m-1" src={cttfn.image} alt="" width={50} height={50} /></Link>
+            <Link key={index} href={cttfn.link} target="_blank" className="border-lime-500 border-8 rounded-full hover:bg-primary hover:scale-110">
+              <Image src={cttfn.image} alt="" width={50} height={50} className="" /></Link>
           ))}
         </div>
       </div>
